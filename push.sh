2f1c8f927d6a096d5897429dc3b803d0f14fad8e
Akihiro TAKASE <42milez@gmail.com>
2020-02-17 21:59:44 +0900

    tiny change

--- push.sh
+++ push.sh
@@ -4,8 +4,8 @@ DOCKER_HUB_ID=42milez
 IMAGE_NAME=cpp-dev-base
 TAG=$(date "+%Y%m%d")
 
-docker build --no-cache -t ${DOCKER_HUB_ID}/${IMAGE_NAME}:${TAG} .
-docker build --no-cache -t ${DOCKER_HUB_ID}/${IMAGE_NAME}:latest .
+docker build --no-cache -t ${IMAGE_NAME} .
+
+docker tag ${IMAGE_NAME} ${DOCKER_HUB_ID}/${IMAGE_NAME}:${TAG} .
 
 docker push ${DOCKER_HUB_ID}/${IMAGE_NAME}:${TAG}
-docker push ${DOCKER_HUB_ID}/${IMAGE_NAME}:latest
